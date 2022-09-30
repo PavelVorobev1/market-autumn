fa450a38610e99fee28efb9b0991069d1f56606c
PavelKazAn <frozzty@yandex.ru>
2022-09-30 15:31:08 +0300

    Initial commit

---
+++ src/main/java/ru/vorobev/marketautumn/MarketAutumnApplication.java
@@ -0,0 +1,13 @@
+package ru.vorobev.marketautumn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAutumnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketAutumnApplication.class, args);
+	}
+
+}
