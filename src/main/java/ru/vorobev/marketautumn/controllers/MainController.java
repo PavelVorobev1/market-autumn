9d5a8f57abf70d086fb9af6fb7fb357241888bf4
PavelKazAn <frozzty@yandex.ru>
2022-10-06 18:17:20 +0300

    Создание отображения информации о продуктах в таблице

--- src/main/java/ru/vorobev/marketautumn/controllers/MainController.java
+++ src/main/java/ru/vorobev/marketautumn/controllers/MainController.java
@@ -1,27 +1,31 @@
 package ru.vorobev.marketautumn.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.vorobev.marketautumn.dto.Product;
 import ru.vorobev.marketautumn.service.ProductService;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
 public class MainController {
 
-    private final ProductService service;
+    @Autowired
+    private ProductService service;
 
     @GetMapping("/get")
-    public String getProduct(Model model, @RequestParam Long id){
+    public String getProduct(Model model, @RequestParam Long id) {
         model.addAttribute("productFront", service.getProduct(id));
         return "index.html";
     }
 
-    @GetMapping("/getall")
-    public String getAllProduct(Model model){
-        model.addAttribute("productList", service.getAllProducts());
-        return "product.html";
+    @GetMapping("/get/all")
+    public List<Product> getAllProduct() {
+        return service.getAllProducts();
     }
 }
