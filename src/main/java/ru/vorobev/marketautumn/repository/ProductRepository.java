19fc8ce3e9e58839f6d467388c16f5c5bb2f3833
PavelKazAn <frozzty@yandex.ru>
2022-10-18 20:42:27 +0300

    Spring Data

--- src/main/java/ru/vorobev/marketautumn/repository/ProductRepository.java
+++ src/main/java/ru/vorobev/marketautumn/repository/ProductRepository.java
@@ -1,36 +1,13 @@
 package ru.vorobev.marketautumn.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.vorobev.marketautumn.dto.Product;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 @Repository
-public class ProductRepository {
-    private List<Product> products;
-
-    @PostConstruct
-    public void init() {
-        products = new ArrayList<>(Arrays.asList(
-                new Product(1L, "Apple", 15),
-                new Product(2L, "Orange", 19),
-                new Product(3L, "Avocado", 30),
-                new Product(4L, "Milk", 10),
-                new Product(5L, "Bread", 5),
-                new Product(6L, "Coffee", 50)
-        ));
-    }
-
-    public Product findById(Long id) {
-        return products.stream().filter(p -> p.getId()==id).findFirst().orElseThrow();
-    }
-
-    public List<Product> getAllProducts(){
-        return products;
-    }
+public interface ProductRepository extends JpaRepository<Product,Long> {
 
 }
